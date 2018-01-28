568184d7881545b3845358e9b8a90e2b9f31ef39
Kev Adsett <kev.adsett@gmail.com>
2018-01-28 12:30:50 +0000

    Add basic customer satisfaction

---
+++ Snail Mail/Assets/Scripts/CustomerSatisfaction.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerSatisfaction : MonoBehaviour
+{
+	public delegate void ZeroSatisfactionAction();
+	public static event ZeroSatisfactionAction OnZeroSatisfaction;
+
+	public float MaxSatisfaction;
+	public float PenaltyForMiss;
+	public float PenaltyForWrongRecipient;
+	public float BonusForRightRecipient;
+	public float SatisfactionDrainRate;
+
+	public Text DummySatisfactionDisplay;
+
+	private float _currentSatisfaction;
+
+	void Start ()
+	{
+		_currentSatisfaction = MaxSatisfaction;
+		Recipient.OnMessageReceived += OnMessageReceived;
+		Launchable.OnFailedToImpact += OnFailedToImpact;
+	}
+
+	void OnDestroy()
+	{
+		Recipient.OnMessageReceived -= OnMessageReceived;
+		Launchable.OnFailedToImpact -= OnFailedToImpact;
+	}
+
+	void Update()
+	{
+		_currentSatisfaction -= SatisfactionDrainRate * Time.deltaTime;
+
+		Mathf.Clamp(_currentSatisfaction, 0, MaxSatisfaction);
+
+		if (_currentSatisfaction <= 0)
+		{
+			if (OnZeroSatisfaction != null)
+			{
+				OnZeroSatisfaction();
+			}
+		}
+
+		DummySatisfactionDisplay.text = _currentSatisfaction.ToString();
+
+	}
+
+	void OnMessageReceived (Recipient recipient, bool isCorrectRecipient)
+	{
+		if (isCorrectRecipient)
+		{
+			_currentSatisfaction += BonusForRightRecipient;
+		}
+		else
+		{
+			_currentSatisfaction -= PenaltyForWrongRecipient;
+		}
+	}
+
+	void OnFailedToImpact (Addressee recipient)
+	{
+		_currentSatisfaction -= PenaltyForMiss;
+	}
+}
