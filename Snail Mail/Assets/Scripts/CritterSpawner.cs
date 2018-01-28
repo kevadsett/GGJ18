af5a389ba02bdc6d40effaaa014e85dffcf875c2
Kev Adsett <kev.adsett@gmail.com>
2018-01-28 13:15:38 +0000

    Have at least one of each critter at any one time

--- Snail Mail/Assets/Scripts/CritterSpawner.cs
+++ Snail Mail/Assets/Scripts/CritterSpawner.cs
@@ -12,37 +12,81 @@ public class CritterSpawner : MonoBehaviour
 	private Camera _mainCamera;
 	private List<GameObject> _spawnedCritters = new List<GameObject>();
 	private bool _waitingToSpawn;
+	private List<string> _prefabNames = new List<string>();
+	private Dictionary<string, GameObject> _namesToPrefab = new Dictionary<string, GameObject>();
+	private Dictionary<string, int> _critterCounts = new Dictionary<string, int>();
 
 	void Start ()
 	{
 		_mainCamera = Camera.main;
 		Recipient.OnMessageReceived += OnMessageReceived;
+
+		CritterPrefabs.ForEach(prefab =>
+		{
+			_prefabNames.Add(prefab.name);
+			_namesToPrefab.Add(prefab.name, prefab);
+			_critterCounts.Add(prefab.name, 0);
+		});
+
 		SpawnCritter();
+	}
+
+	void OnDestroy()
+	{
+		Recipient.OnMessageReceived -= OnMessageReceived;
 	}
 
 	void Update()
 	{
+		for (var i = 0; i < _prefabNames.Count; i++)
+		{
+			string prefabName = _prefabNames[i];
+			int count;
+			_critterCounts.TryGetValue(prefabName, out count);
+			if (count == 0)
+			{
+				StartCoroutine(WaitAndSpawn(prefabName, 0.5f));
+			}
+		}
+
 		if (_spawnedCritters.Count < MaxCritters && !_waitingToSpawn)
 		{
 			StartCoroutine(WaitAndSpawn());
 		}
 	}
 
-	private IEnumerator WaitAndSpawn()
+	private IEnumerator WaitAndSpawn(string prefabName = null, float wait = -1)
 	{
 		_waitingToSpawn = true;
-		var waitTime = Random.value * MaxWait;
+		var waitTime = wait < 0 ? Random.value * MaxWait : wait;
+
+		if (string.IsNullOrEmpty(prefabName) == false)
+		{
+			_critterCounts[prefabName]++;
+		}
+
 		yield return new WaitForSeconds(waitTime);
 
-		SpawnCritter();
+		SpawnCritter(prefabName);
 		_waitingToSpawn = false;
 	}
 
-	private void SpawnCritter()
+	private void SpawnCritter(string prefabName = null)
 	{
-		var selectedPrefab = CritterPrefabs[Random.Range(0, CritterPrefabs.Count)];
+		GameObject selectedPrefab = null;
+		if (string.IsNullOrEmpty(prefabName) == false)
+		{
+			selectedPrefab = _namesToPrefab[prefabName];
+		}
+		else
+		{
+			selectedPrefab = CritterPrefabs[Random.Range(0, CritterPrefabs.Count)];
+			_critterCounts[selectedPrefab.name]++;
+		}
+
 		var selectedSpawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Count)];
 		var critterObject = Instantiate(selectedPrefab);
+		critterObject.name = selectedPrefab.name;
 		critterObject.transform.position = new Vector3(
 			selectedSpawnPoint.position.x,
 			selectedSpawnPoint.position.y,
@@ -58,6 +102,7 @@ public class CritterSpawner : MonoBehaviour
 
 		if (_spawnedCritters.Contains(recipientGameObject))
 		{
+			_critterCounts[recipientGameObject.name]--;
 			_spawnedCritters.Remove(recipientGameObject);
 		}
 	}
