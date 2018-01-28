071bbfde46933e865157cf54c40175310df33e8b
gav <notoriousknave@gmail.com>
2018-01-28 02:40:05 +0000

    throb

---
+++ Snail Mail/Assets/Scripts/Throb.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throb : MonoBehaviour {
+	[SerializeField] float scale;
+	[SerializeField] float speed;
+
+	float timer;
+
+	void Update () {
+		transform.localScale = Vector3.one * (1f + Mathf.Sin (timer) * scale);
+		timer = Mathf.Repeat (timer + Time.deltaTime * speed, Mathf.PI * 2f);
+	}
+}
