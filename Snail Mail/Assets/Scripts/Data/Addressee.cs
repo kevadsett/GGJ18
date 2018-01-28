5443b4ec9e5ac3610377fc3875a8f76ef01139e0
gav <notoriousknave@gmail.com>
2018-01-28 01:12:34 +0000

    me and good commit practise are super not aligned right now

---
+++ Snail Mail/Assets/Scripts/Data/Addressee.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Addressee : ScriptableObject {
+	public string forename;
+	public string surname;
+	public string addressline1;
+	public string addressline2;
+
+	public string Address { get {
+		return addressline1 + "\n" + addressline2;
+	} }
+}
---
+++ Snail Mail/Assets/Scripts/Data/GameDatabase.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameDatabase : ScriptableObject {
+	public Addressee[] addressees;
+}
